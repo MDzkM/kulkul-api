94325f8b1c184db5108a26c5ee8e32f46f73a0be
Muhammad Dzikra Muzaki <mdzkm17@gmail.com>
2022-08-19 10:30:11 +0700

    Add AWS S3 connection for image backup

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go v1.44.80 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -15,7 +17,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- handlers/task.go
+++ handlers/task.go
@@ -8,9 +8,53 @@ import (
 	"github.com/MDzkM/kulkul-api/models"
 
 	"github.com/labstack/echo"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/credentials"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 type H map[string]interface{}
+
+func initAWSConnection() *s3.S3 {
+	awsAccessKey = "AKIAW74DYNZLAVCSJQMC"
+	awsSecretKey = "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"
+
+	creds = credentials.NewStaticCredentials(awsAccessKey, awsSecretKey, "")
+
+	_, err = creds.Get()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	s3Region = "us-east-1"
+
+	cfg = aws.NewConfig().WithRegion(s3Region).WithCredentials(creds)
+
+	s3Connection = s3.New(session.New(), cfg)
+	return s3Connection
+}
+
+func S3UploadBase64(base64File string, objectKey string) error {
+	decode, err = base64.StdEncoding.DecodeString(base64File)
+
+	if err != nil {
+		return err
+	}
+
+	awsSession = initAWSConnection()
+
+	uploadParams = &s3.PutObjectInput{
+		Bucket: aws.String("kulkul"),
+		Key:    aws.String(objectKey),
+		Body:   bytes.NewReader(decode),
+	}
+
+	_, err = awsSession.PutObject(uploadParams)
+
+	return err
+}
 
 func GetFridges(db *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
@@ -25,8 +69,19 @@ func PutFridge(db *sql.DB) echo.HandlerFunc {
 
 		c.Bind(&fridge)
 
-		id, err := models.PutFridge(db, fridge.Model, fridge.Owner, fridge.Image)
+		id, err = models.PutFridge(db, fridge.Model, fridge.Owner, fridge.Image)
 
+		keyName = "fridge-" + strconv.Itoa(id) + ".jpg"
+		
+		b64data = base64Image[strings.IndexByte(base64Image, ',')+1:]
+
+		err = services.S3UploadBase64(b64data, keyName)
+		
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		return
 		if err == nil {
 			return c.JSON(http.StatusCreated, H{
 				"created": id,
